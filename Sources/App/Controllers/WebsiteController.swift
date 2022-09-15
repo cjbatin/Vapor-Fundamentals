ced00663532e825cde22f10f96c161e2ceb57ba4
Christopher Batin <christopher.batin@and.digital>
2022-09-15 10:02:49 +0100

    Added homepage
    Shows all rooms in a basic table

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -1,8 +1,27 @@
 //
-//  File.swift
+//  WebsiteController.swift
 //  
 //
 //  Created by Christopher Batin on 09/09/2022.
 //
 
-import Foundation
+import Vapor
+import Leaf
+
+struct WebsiteController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        routes.get(use: indexHandler)
+    }
+
+    struct IndexContext: Encodable {
+        let title: String
+        let rooms: [Room]
+    }
+
+    func indexHandler(_ req: Request) async throws -> View {
+        let rooms = try await Room.query(on: req.db).all()
+        let context = IndexContext(title: "Rooms", rooms: rooms)
+        return try await req.view.render("index", context)
+    }
+}
+
